7a4a08e94080bda7fc58844809f1573d4d1788c7
Joseph P Bailey <jpbailey@JPB-in-VMH.local>
2020-06-13 19:22:09 -0400

    updating submit button

--- editnames.php
+++ editnames.php
@@ -37,7 +37,8 @@ while ($row=$vc_query->fetch_assoc()) {
     $user_row=$user_query->fetch_assoc();
     echo "<tr>";
     echo "<td>".$user_row['name']."</td>";
-    echo "<td><input type=text name='newname' value=".$user_row['name']."></td>";
+    echo "<td><form><input type=text name='newname' value='".$user_row['name']."'>";
+    echo "<input type='submit' name='edit_name' value='save'></td>";
     $vc_url = "http://digitalplatformgames.com/games/login.php?event_id=".$event_id."&user_id=".$user_row['id'];
     echo "<td><a href='".$vc_url."'>".$vc_url."</a></td>";
     echo "<td>".$user_row['password']."</td>";
