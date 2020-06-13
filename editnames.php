6ca058d1acf59195113155001332b8e96b8922c0
Joseph P Bailey <jpbailey@JPB-in-VMH.local>
2020-06-13 17:42:25 -0400

    making table

--- editnames.php
+++ editnames.php
@@ -18,7 +18,7 @@ $sql = "SELECT DISTINCT startup_id FROM bid WHERE event_id=".$event_id;
 $startup_query = $mysqli->query($sql);
 
 while ($row=$startup_query->fetch_assoc()) {
-    $sql = "SELECT name FROM user where id=".$row['startup_id'];
+    $sql = "SELECT name, password FROM user where id=".$row['startup_id'];
     $user_query=$mysqli->query($sql);
     $user_row=$user_query->fetch_assoc();
     echo $user_row['name'];
@@ -29,13 +29,20 @@ echo "<h2>VCs</h2>\n";
 $sql = "SELECT DISTINCT vc_id FROM bid WHERE event_id=".$event_id;
 $vc_query = $mysqli->query($sql);
 
+echo "<table>";
+echo "<tr><th>name</th><th>password</th><th>URL</th><th>action</th></tr>\n";
 while ($row=$vc_query->fetch_assoc()) {
-    $sql = "SELECT name FROM user where id=".$row['vc_id'];
+    $sql = "SELECT * FROM user where id=".$row['vc_id'];
     $user_query=$mysqli->query($sql);
     $user_row=$user_query->fetch_assoc();
-    echo $user_row['name'];
-    echo "<br>\n";
+    echo "<tr>";
+    echo "<td>".$user_row['name']."</td>";
+    echo "<td>".$user_row['password']."</td>";
+    echo "<td>".$user_row['id']."</td>";
+    echo "<td>edit</td>";
+    echo "</tr>";
 }
+echo "</table>\n";
 
 
 ?>
