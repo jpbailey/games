8e9aff8877a0fb29593d00f0faa9323c95c569fc
Joseph P Bailey <jpbailey@JPB-in-VMH.local>
2020-06-13 17:32:42 -0400

    mapping names to ids

--- editnames.php
+++ editnames.php
@@ -13,16 +13,29 @@ require ('./variables.php');
 // make, check, and authenticate database connection
 require './database.php';
 
-
+echo "<h2>Startups</h2>\n";
 $sql = "SELECT DISTINCT startup_id FROM bid WHERE event_id=".$event_id;
 $startup_query = $mysqli->query($sql);
 
 while ($row=$startup_query->fetch_assoc()) {
-    echo $row['startup_id'];
+    $sql = "SELECT name FROM user where id=".$row['startup_id'];
+    $user_query=$mysqli->query($sql);
+    $user_row=$user_query->fetch_assoc();
+    echo $user_row['name'];
+    echo "<br>\n";
 }
 
+echo "<h2>VCs</h2>\n";
+$sql = "SELECT DISTINCT vc_id FROM bid WHERE event_id=".$event_id;
+$vc_query = $mysqli->query($sql);
 
-//$sql = "SELECT DISTINCT vc_id FROM bid WHERE event_id=".$event_id;
+while ($row=$vc_query->fetch_assoc()) {
+    $sql = "SELECT name FROM user where id=".$row['vc_id'];
+    $user_query=$mysqli->query($sql);
+    $user_row=$user_query->fetch_assoc();
+    echo $user_row['name'];
+    echo "<br>\n";
+}
 
 
 ?>
