9258325ec57fbd8a1937a7b525b1e42be1f0e501
Joseph P Bailey <jpbailey@JPB-in-VMH.local>
2020-06-13 19:17:26 -0400

    making input text box

--- editnames.php
+++ editnames.php
@@ -30,13 +30,14 @@ $sql = "SELECT DISTINCT vc_id FROM bid WHERE event_id=".$event_id;
 $vc_query = $mysqli->query($sql);
 
 echo "<table id='gamedata'>";
-echo "<tr><th>name</th><th>URL</th><th>password</th><th>action</th></tr>\n";
+echo "<tr><th>name</th><th>new name</th><th>URL</th><th>password</th><th>action</th></tr>\n";
 while ($row=$vc_query->fetch_assoc()) {
     $sql = "SELECT * FROM user where id=".$row['vc_id'];
     $user_query=$mysqli->query($sql);
     $user_row=$user_query->fetch_assoc();
     echo "<tr>";
     echo "<td>".$user_row['name']."</td>";
+    echo "<input type=text name='newname' value=".$user_row['name'].">";
     $vc_url = "http://digitalplatformgames.com/games/login.php?event_id=".$event_id."&user_id=".$user_row['id'];
     echo "<td><a href='".$vc_url."'>".$vc_url."</a></td>";
     echo "<td>".$user_row['password']."</td>";
