9301e29aed0609272da278afad7b3861f41be6b5
Joseph P Bailey <jpbailey@JPB-in-VMH.local>
2020-06-13 19:29:15 -0400

    adding startup table

--- editnames.php
+++ editnames.php
@@ -17,6 +17,26 @@ echo "<h2>Startups</h2>\n";
 $sql = "SELECT DISTINCT startup_id FROM bid WHERE event_id=".$event_id;
 $startup_query = $mysqli->query($sql);
 
+echo "<table id='gamedata'>";
+echo "<tr><th>name</th>";
+//echo "<th>new name</th>";
+echo "<th>URL</th><th>password</th><th>action</th></tr>\n";
+while ($row=$vc_query->fetch_assoc()) {
+    $sql = "SELECT * FROM user where id=".$row['startup_id'];
+    $user_query=$mysqli->query($sql);
+    $user_row=$user_query->fetch_assoc();
+    echo "<tr>";
+    echo "<td>".$user_row['name']."</td>";
+//    echo "<td><input type=text name='newname' value='".$user_row['name']."'>";
+//    echo "<input type='submit' name='edit_name' value='save'></td>";
+    $startup_url = "http://digitalplatformgames.com/games/login.php?event_id=".$event_id."&user_id=".$user_row['id'];
+    echo "<td><a href='".$startup_url."'>".$startup_url."</a></td>";
+    echo "<td>".$user_row['password']."</td>";
+    echo "<td>n/a</td>";
+    echo "</tr>";
+}
+echo "</table>\n";
+
 while ($row=$startup_query->fetch_assoc()) {
     $sql = "SELECT name, password FROM user where id=".$row['startup_id'];
     $user_query=$mysqli->query($sql);
@@ -30,19 +50,21 @@ $sql = "SELECT DISTINCT vc_id FROM bid WHERE event_id=".$event_id;
 $vc_query = $mysqli->query($sql);
 
 echo "<table id='gamedata'>";
-echo "<tr><th>name</th><th>new name</th><th>URL</th><th>password</th><th>action</th></tr>\n";
+echo "<tr><th>name</th>";
+//echo "<th>new name</th>";
+echo "<th>URL</th><th>password</th><th>action</th></tr>\n";
 while ($row=$vc_query->fetch_assoc()) {
     $sql = "SELECT * FROM user where id=".$row['vc_id'];
     $user_query=$mysqli->query($sql);
     $user_row=$user_query->fetch_assoc();
     echo "<tr>";
     echo "<td>".$user_row['name']."</td>";
-    echo "<td><form action='><input type=text name='newname' value='".$user_row['name']."'>";
-    echo "<input type='submit' name='edit_name' value='save'></td>";
+//    echo "<td><input type=text name='newname' value='".$user_row['name']."'>";
+//    echo "<input type='submit' name='edit_name' value='save'></td>";
     $vc_url = "http://digitalplatformgames.com/games/login.php?event_id=".$event_id."&user_id=".$user_row['id'];
     echo "<td><a href='".$vc_url."'>".$vc_url."</a></td>";
     echo "<td>".$user_row['password']."</td>";
-    echo "<td>edit</td>";
+    echo "<td>n/a</td>";
     echo "</tr>";
 }
 echo "</table>\n";
