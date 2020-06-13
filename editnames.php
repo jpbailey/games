be8749d14808888daade289e2146cc89335ef740
Joseph P Bailey <jpbailey@JPB-in-VMH.local>
2020-06-13 17:45:25 -0400

    improving url

--- editnames.php
+++ editnames.php
@@ -29,16 +29,17 @@ echo "<h2>VCs</h2>\n";
 $sql = "SELECT DISTINCT vc_id FROM bid WHERE event_id=".$event_id;
 $vc_query = $mysqli->query($sql);
 
-echo "<table>";
-echo "<tr><th>name</th><th>password</th><th>URL</th><th>action</th></tr>\n";
+echo "<table id='gamedata'>";
+echo "<tr><th>name</th><th>URL</th><th>password</th><th>action</th></tr>\n";
 while ($row=$vc_query->fetch_assoc()) {
     $sql = "SELECT * FROM user where id=".$row['vc_id'];
     $user_query=$mysqli->query($sql);
     $user_row=$user_query->fetch_assoc();
     echo "<tr>";
     echo "<td>".$user_row['name']."</td>";
+    $url = "http://digitalplatformgames.com/games/login.php?event_id=&user_id=".$user_row['id'];
+    echo "<td>".$url."</td>";
     echo "<td>".$user_row['password']."</td>";
-    echo "<td>".$user_row['id']."</td>";
     echo "<td>edit</td>";
     echo "</tr>";
 }
