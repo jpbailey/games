1715e78af20dbe01f9a94ff195977596f6248f3f
Joseph P Bailey <jpbailey@JPB-in-VMH.local>
2020-06-14 08:48:40 -0400

    fix html formatting

--- sendvars.php
+++ sendvars.php
@@ -1,8 +1,8 @@
 <?php
 
-echo "<input type='hidden' name='event_id' value=$event_id>";
-echo "<input type='hidden' name='user_id' value=$user_id>";
-echo "<input type='hidden' name='password' value=$password>";
+echo "<input type='hidden' name='event_id' value=$event_id>\n";
+echo "<input type='hidden' name='user_id' value=$user_id>\n";
+echo "<input type='hidden' name='password' value=$password>\n";
 
 ?>
 
