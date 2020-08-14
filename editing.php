c51c66302cd06536cd6662874b1c870b6386b98e
Joe Bailey <joebailey@JPB-iMac.local>
2020-08-14 15:36:43 -0400

    changing editing function

--- editing.php
+++ editing.php
@@ -16,23 +16,15 @@ require ('./environment.php');
 // bring in variables
 require ('./variables.php');
 
-if ($_POST['action']=='submit new bid') {
-	$action="newbid";
-} else {
-	$bid_id = substr(key($_POST),6)+0;
-	$action = substr(key($_POST),0,6);
-}
-
-$role=htmlspecialchars($_POST['role']);
-$remaining=htmlspecialchars($_POST['remaining']);
-
 // make, check, and authenticate database connection
 require ('./database.php');
 
 // use the database to bring in the values associated with the
 // current bid information
 
+echo "got here";
 
+echo "what about here";
 
 ?>
 
