fc6c8b256148711c1bc77179f28dea7496271c77
Joe Bailey <joebailey@JPB-iMac.local>
2020-08-14 16:50:04 -0400

    adding edit buttons for vcs

--- editnames.php
+++ editnames.php
@@ -20,7 +20,7 @@ $startup_query = $mysqli->query($sql);
 echo "<table id='gamedata'>";
 echo "<tr><th>name</th>";
 //echo "<th>new name</th>";
-echo "<th>URL</th><th>password</th><th>action</th></tr>\n";
+echo "<th>URL</th><th>password</th></tr>\n";
 while ($row=$startup_query->fetch_assoc()) {
     $sql = "SELECT * FROM user where id=".$row['startup_id'];
     $user_query=$mysqli->query($sql);
@@ -38,7 +38,6 @@ while ($row=$startup_query->fetch_assoc()) {
     $startup_url = "http://digitalplatformgames.com/games/login.php?event_id=".$event_id."&user_id=".$user_row['id'];
     echo "<td><a href='".$startup_url."'>".$startup_url."</a></td>";
     echo "<td>".$user_row['password']."</td>";
-    echo "<td>n/a</td>";
     echo "</tr>";
 }
 echo "</table>\n";
@@ -58,19 +57,24 @@ $vc_query = $mysqli->query($sql);
 echo "<table id='gamedata'>";
 echo "<tr><th>name</th>";
 //echo "<th>new name</th>";
-echo "<th>URL</th><th>password</th><th>action</th></tr>\n";
+echo "<th>URL</th><th>password</th></tr>\n";
 while ($row=$vc_query->fetch_assoc()) {
     $sql = "SELECT * FROM user where id=".$row['vc_id'];
     $user_query=$mysqli->query($sql);
     $user_row=$user_query->fetch_assoc();
     echo "<tr>";
-    echo "<td>".$user_row['name']."</td>";
+    echo "<td>".$user_row['name'];
+    echo "<form action='editing.php' method='POST'>";
+    echo "<input type='submit' name='action' value='edit'>";
+    echo "<input type='hidden' name='edit_id' value=".$row['startup_id'].">";
+    require ('./sendvars.php');
+    echo "</form>";
+    echo "</td>";
 //    echo "<td><input type=text name='newname' value='".$user_row['name']."'>";
 //    echo "<input type='submit' name='edit_name' value='save'></td>";
     $vc_url = "http://digitalplatformgames.com/games/login.php?event_id=".$event_id."&user_id=".$user_row['id'];
     echo "<td><a href='".$vc_url."'>".$vc_url."</a></td>";
     echo "<td>".$user_row['password']."</td>";
-    echo "<td>n/a</td>";
     echo "</tr>";
 }
 echo "</table>\n";
