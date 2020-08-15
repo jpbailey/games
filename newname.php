078b31a5bfe2fd567b4c53906ca47991cdd3b5b8
Joe Bailey <joebailey@JPB-iMac.local>
2020-08-15 10:29:46 -0400

    using prepare and bind_param for sql update

--- newname.php
+++ newname.php
@@ -43,10 +43,19 @@ echo "new name is: ".$newname."<p>";
 // check to make sure new name is sql compliant
 // no sql injections here!
 
-$sql = "UPDATE user SET name='".$newname."' WHERE id=".$edit_id;
-$clean_sql = $mysqli->prepare($sql);
+//$sql = "UPDATE user SET name='".$newname."' WHERE id=".$edit_id;
+$stmt = $mysqli->prepare( "UPDATE user SET name= ? WHERE id= ?");
+$stmt->bind_param("si", $newname, $edit_id);
+$stmt->execute();
+$stmt->close();
 
-echo $clean_sql;
+//$stmt = $mysqli->prepare("UPDATE myTable SET name = ? WHERE id = ?");
+//$stmt->bind_param("si", $_POST['name'], $_SESSION['id']);
+
+//$stmt = $conn->prepare("INSERT INTO MyGuests (firstname, lastname, email) VALUES (?, ?, ?)");
+//$stmt->bind_param("sss", $firstname, $lastname, $email);
+
+//echo $clean_sql;
 //$mysqli->query($sql);
 
 echo "<form action='editnames.php' method='POST'>";
